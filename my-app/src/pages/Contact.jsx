4fad415d226e0474c7f668a943f3d989188e6f54
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-02-28 02:30:46 -0500

    added socials to contact me

--- my-app/src/pages/Contact.jsx
+++ my-app/src/pages/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import emailjs from 'emailjs-com';
 import "../styles/Contactme.css"
+import {FaLinkedin, FaGithub, FaYoutube} from "react-icons/fa"
+
 
 const Contact = () => {
 
@@ -20,19 +22,30 @@ const Contact = () => {
   return (
     <section id="contact-me">
         <div className = "container">
-          <h1 className = "heading">Contact Me</h1>
-          <form onSubmit={sendEmail}>
-              {/* <label>Name</label> */}
-              <input className = 'input' placeholder = "Enter Name" type="text" name="name" />
-              {/* <label>Name</label> */}
-              <input className = 'input' placeholder = "Enter Subject" type="text" name="subject" />
-              {/* <label>Subject</label> */}
-              <input className = 'input' placeholder = "Enter Email" type="email" name="email" />
-              {/* <label>Message</label> */}
-              <textarea className = "mesg" type="text" placeholder = "Enter Message" name="message" />
-              <input type="submit"  className = "submit" placeholder = "Enter Message" value="Send" />
-           </form>
 
+          <div>
+            <h1 className = "heading">Contact Me</h1>
+            <h3 className='email'>luismolinuevo0@gmail.com</h3>
+            <div className='socialContain'>
+              <ul>
+                <li><FaLinkedin className='contactIcons'/></li>
+                <li><FaGithub className='contactIcons'/></li>
+                <li><FaYoutube className='contactIcons'/></li>
+              </ul>
+            </div>
+
+            <form onSubmit={sendEmail}>
+                {/* <label>Name</label> */}
+                <input className = 'input' placeholder = "Enter Name" type="text" name="name" />
+                {/* <label>Name</label> */}
+                <input className = 'input' placeholder = "Enter Subject" type="text" name="subject" />
+                {/* <label>Subject</label> */}
+                <input className = 'input' placeholder = "Enter Email" type="email" name="email" />
+                {/* <label>Message</label> */}
+                <textarea className = "mesg" type="text" placeholder = "Enter Message" name="message" />
+                <input type="submit"  className = "submit" placeholder = "Enter Message" value="Send" />
+            </form>
+          </div>
         </div>
     </section>
   )
