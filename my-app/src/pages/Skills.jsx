27f46475c3b90ab2367d5a1d049cdcd4350c99cc
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-03-01 15:50:07 -0500

    fixed about me and added skills

--- my-app/src/pages/Skills.jsx
+++ my-app/src/pages/Skills.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import "../styles/skills.css"
 import {FaReact, FaBootstrap, FaGitSquare} from "react-icons/fa";
-import {DiJava, DiJavascript1, DiCss3, DiHtml5, DiPython } from "react-icons/di"
+import {DiJava, DiJavascript1, DiCss3, DiHtml5, DiPython, DiMongodb, DiNodejs } from "react-icons/di"
+import {SiSequelize, SiExpress, SiPrisma} from "react-icons/si"
 import { Slide } from "react-awesome-reveal";
 
 
@@ -12,17 +13,17 @@ const Skills = () => {
       <div className='container'>
         <h1 className="heading">Skills</h1>
         <div className = "skills-main">
-          <Slide damping={0.1} direction={"left"}>
+          <Slide damping={0.1} direction={"left"} triggerOnce="true">
             <div className="about">
               <p className = "accent">Hello!</p>
               <p>My name is Luis Molinuevo a Web Developer. 
-                Im currently a Computer Science senior at Lehman College. 
-                Im also in The Knowledge House Innovation and Cuny Tech Prep Fellowships.
-                I use modern technologies like React to build fully responsive full stack applications.
-                I also love to upscale my skills and problem solve.</p>
+                I'm currently a Computer Science senior at Lehman College. 
+                I'm also in The Knowledge House Innovation and Cuny Tech Prep Fellowships.
+                I use modern technologies like React and Express to build real world Full Stack applications.
+              </p>
             </div>
           </Slide>
-          <Slide damping={0.2} direction={"left"}>
+          <Slide damping={0.1} direction={"left"} triggerOnce="false">
             <div className = "skills">
               {/* <img className = "test" src={hey}/> */}
               
@@ -30,13 +31,33 @@ const Skills = () => {
                 <FaReact/>
                 <p>React.js</p>
               </div>
-              <div className = "skill">
-                <DiJava/>
-                <p>Java</p>
+              <div className='skill'>
+                <SiExpress/>
+                <p>Express</p>
               </div>
+              <div className = "skill">
+                <DiNodejs/>
+                <p>NodeJS</p>
+              </div>
+              <div className = "skill">
+                <DiMongodb/>
+                <p>Mongo</p>
+              </div>
+              <div className='skill'>
+                <SiPrisma/>
+                <p>Prisma</p>
+              </div>
+              {/* <div className = "skill">
+                <SiSequelize/>
+                <p>Sequelize</p>
+              </div> */}
               <div className = "skill">
                 <DiJavascript1/>
                 <p>JS</p>
+              </div>
+              <div className = "skill">
+                <DiJava/>
+                <p>Java</p>
               </div>
               <div className = "skill">
                 <DiHtml5/>
@@ -46,14 +67,14 @@ const Skills = () => {
                 <DiCss3/>
                 <p>CSS</p>
               </div>
-              <div className = "skill">
+              {/* <div className = "skill">
                 <DiPython/>
                 <p>Python</p>
-              </div>
-              <div className = "skill">
+              </div> */}
+              {/* <div className = "skill">
                 <FaBootstrap/>
                 <p>Bootstrap</p>
-              </div>
+              </div> */}
               <div className = "skill">
                 <FaGitSquare/>
                 <p>Git</p>
